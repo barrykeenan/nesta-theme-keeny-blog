0703e48f1ca41d15fba867d83b9cadbc96ea8a16
Barry Keenan <me@keeny.co.uk>
2010-11-21 13:09:55 +1300

    app changes

--- app.rb
+++ app.rb
@@ -8,39 +8,37 @@
 #
 # use Rack::Static, :urls => ["/keeny"], :root => "themes/keeny/public"
 
-class Page < FileModel
-  module ClassMethods
-    def nav_items
-      LOGGER.info "nav items"
-      menu = Nesta::Config.content_path("navigation.txt")
-      pages = []
-      if File.exist?(menu)
-        File.open(menu).each { |line| pages << Page.load(line.chomp) }
-      end
-      pages
-    end
-  end
-  
-  def hide_comments
-    @hide_comments = metadata("hide comments")? metadata("hide comments") : false;
-  end
-  
-  def menu_title
-    @menu_title = metadata("menu title")? metadata("menu title") : "";
-  end
-  
-end
-
-
-helpers do
-  
-  def set_common_variables
-    logger.info "vars"
-    @menu_items = Page.menu_items
-    @nav_items = Page.nav_items
-    @site_title = Nesta::Config.title
-    set_from_config(:title, :subtitle, :google_analytics_code)
-    @heading = @title
-  end
-  
-end
+# class Page < FileModel
+#   module ClassMethods
+#     def nav_items
+#       menu = Nesta::Config.content_path("navigation.txt")
+#       pages = []
+#       if File.exist?(menu)
+#         File.open(menu).each { |line| pages << Page.load(line.chomp) }
+#       end
+#       pages
+#     end
+#   end
+#   
+#   def hide_comments
+#     @hide_comments = metadata("hide comments")? metadata("hide comments") : false;
+#   end
+#   
+#   def menu_title
+#     @menu_title = metadata("menu title")? metadata("menu title") : "";
+#   end
+#   
+# end
+# 
+# 
+# helpers do
+#   
+#   def set_common_variables
+#     @menu_items = Page.menu_items
+#     @nav_items = Page.nav_items
+#     @site_title = Nesta::Config.title
+#     set_from_config(:title, :subtitle, :google_analytics_code)
+#     @heading = @title
+#   end
+#   
+# end
